91d1c0eec4b57c807521e1841ea57bdb27dd49e1
unknown <hughesgeorgeking@gmail.com>
2023-09-03 16:37:11 +0000

    changes

--- bin/console_project.dart
+++ bin/console_project.dart
@@ -1,5 +1,12 @@
 import 'package:console_project/console_project.dart' as console_project;
+import 'package:console_project/src/calculate.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${console_project.calculate()}!');
+
+  // Printing my calculator
+  print("Add: ${calculateAdd()}");
+  print("Subtract:  ${calculateSubtract()}");
+  print("Multiply: ${calculateMultiply()}");
+  print("Divide: ${calculateDivide()}");
 }
