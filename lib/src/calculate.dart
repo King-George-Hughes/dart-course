44fc442155603c55ebdafe3ee6ca715412be6010
unknown <hughesgeorgeking@gmail.com>
2023-09-03 16:31:53 +0000

    first commit

---
+++ lib/src/calculate.dart
@@ -0,0 +1,17 @@
+import 'package:calculator/calculator.dart';
+
+int calculateAdd() {
+  return add(25, 5);
+}
+
+int calculateSubtract() {
+  return subtract(25, 5);
+}
+
+double calculateDivide() {
+  return divide(25, 5);
+}
+
+int calculateMultiply() {
+  return multiply(5, 5);
+}
